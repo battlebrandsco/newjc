1f7b4a5b15764d77ccffd6f1719b7a375939634f
Marcus Battle <themarcusbattle@gmail.com>
2018-09-16 21:44:24 -0400

    Update layout for outreach page

--- themes/newjc/page-outreach.php
+++ themes/newjc/page-outreach.php
@@ -10,22 +10,22 @@
     </div>
 </section>
 <section>
-    <div class="grid has-width-75 is-vertical-on-mobile">
-        <div class="col">
-            <iframe width="640" height="290" style="border-width:1px; border-style:solid; border-color:#e6e6e6;" src="https://media.wfmynews2.com/embeds/video/8099186/iframe" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true"></iframe>
+    <div class="grid has-width-75 is-full-on-mobile is-vertical-on-mobile vertical-align">
+        <div class="col has-padding">
+            <iframe width="640" height="290" src="https://media.wfmynews2.com/embeds/video/8099186/iframe" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true"></iframe>
         </div>
-        <div class="col">
+        <div class="col has-padding">
             <h2>Greensboro Tornado Relief</h2>
             <p>On Sunday, April 15, 2018 an EF-2 Tornado with winds reaching 135mph and a width of at least 300 yards touched down in Greensboro. Sitting back and doing nothing wasn't an option. As Christ followers, we are committed to making sure that whenever we're in a position to help, we give compassionately and liberally. It's not enough for us to preach that Jesus saves, we as his students, we must live the Word.</p>
         </div>
     </div>
 </section>
 <section>
-    <div class="grid has-width-75 is-vertical-on-mobile">
+    <div class="grid has-text-align-center has-width-75 is-full-on-mobile is-vertical-on-mobile has-text-align-left-on-mobile">
         <div class="col">
             <h2>Every Soul Matters</h2>
             <p> With every soul being of the upmost importance, C.O.R.E provides hands on training for evangelism. We believe that it is necessary for every believer to become equipped with tools to share the Gospel of Jesus Christ all while serving our brothers and sisters in excellence. If you have a passion for sharing the gospel in non-traditional ways then we believe that C.O.R.E is a great place for you and would love for you to be apart of our ever growing outreach team.</p>
-            <a class="btn">Join C.O.R.E.</a>
+            <a class="btn" href="<?php echo home_url('forms/join-core'); ?>">Join C.O.R.E.</a>
         </div>
     </div>
 </section>
