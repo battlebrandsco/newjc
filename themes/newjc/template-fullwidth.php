f9763eef4d4a5b334479ffe9e013e1d924a6af87
Marcus Battle <themarcusbattle@gmail.com>
2018-09-23 02:22:34 -0400

    Add full width template

---
+++ themes/newjc/template-fullwidth.php
@@ -0,0 +1,22 @@
+<?php 
+    /** Template Name: Full Width */
+    get_header(); 
+?>
+
+<section>
+    <div class="hero">
+        <div class="hero-panel">
+            <h1><?php the_title(); ?></h1>
+        </div>
+    </div>
+</section>
+
+<section id="main">
+    <div class="container">
+        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+            <?php the_content(); ?>
+        <?php endwhile; endif; ?>
+    </div>
+</section>
+
+<?php get_footer(); ?>
