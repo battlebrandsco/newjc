956b394614d789816b28369048ade906f8142deb
Marcus Battle <themarcusbattle@gmail.com>
2018-09-23 03:04:15 -0400

    Added padding to get involved page

--- themes/newjc/page-get-involved.php
+++ themes/newjc/page-get-involved.php
@@ -13,15 +13,15 @@
 </section>
 <section>
     <div class="container has-half">
-        <div class="grid has-padding vertical-align is-vertical-on-mobile">
-            <div class="col">
+        <div class="grid vertical-align is-vertical-on-mobile">
+            <div class="col has-padding">
                 <h2>Children</h2>
                 <h3>Every Sunday Morning at 11:00 AM</h3>
                 <p>Welcome to Kingdom Sparks Children’s Ministry! We are so excited about serving you and your child(ren). We are committed to providing an atmosphere of love, respect, compassion and fun while leading in engaging worship experiences weekly. We believe that we play an important role in building each child’s Biblical foundation to prepare their faith for any environment.</p>
                 <a class="btn btn-outlined red" href="<?php echo home_url( 'children' ); ?>">Learn More</a>
                 <a class="btn btn-outlined red" href="<?php echo home_url( 'forms/child-waiver' ); ?>">PRE-REGISTER</a>
             </div>
-            <div class="col">
+            <div class="col has-padding">
                 <div class="img-wrap img-4-x-3">
                     <img src="<?php echo get_stylesheet_directory_uri() . "/assets/images/kingdom-sparks-jump-roping.jpg"; ?>" />
                 </div>
@@ -31,13 +31,13 @@
 </section>
 <section>
     <div class="container has-half">
-        <div class="grid has-padding vertical-align is-vertical-on-mobile">
-            <div class="col">
+        <div class="grid vertical-align is-vertical-on-mobile">
+            <div class="col has-padding">
                 <div class="img-wrap img-4-x-3">
                     <img src="<?php echo get_stylesheet_directory_uri() . "/assets/images/black-boy-smiling-in-front-of-wall.jpg"; ?>" />
                 </div>
             </div>
-            <div class="col">
+            <div class="col has-padding">
                 <h2>Middle &amp; High School</h2>
                 <h3>Every Monday Evening at 7:00 PM</h3>
                 <p>New Jerusalem Cathedral Youth Church (NJCYC) delivers a tailor made experience for youth to be able to express themselves in a fun and functional Christian environment. The Youth Church is designed to equip young people with the necessary tools to be able to stand strong against peer pressure and be/remain saved in an ever changing social environment. We seek to empower today’s youth to win in life, school, on the field, or wherever they are, by equipping them with strong Christian principles and values, through a potent, relevant, and “in your face” gospel that does not compromise the integrity of the Word of God. Utilizing the tools of the Word of God and uplifting social activities we endeavor to inspire youth to develop their God given talents and abilities while living in and on purpose and fulfilling destiny.</p>
@@ -49,13 +49,13 @@
 <section class="has-padded-height">
     <div class="container has-half">
         <div class="grid vertical-align is-vertical-on-mobile">
-            <div class="col">
+            <div class="col has-padding">
                 <h2>College &amp; Young Adults</h2>
                 <h3>Every 1st & 3rd Wednesday Evening at 7:00 PM</h3>
-                <p>A community of young adults going after Jesus.</p>
+                <p>A community of college students and young adults learning how to live and lead others closer to Christ.</p>
                 <a class="btn btn-outlined red" href="https://instagram.com/thesummitnjc">Learn More</a>
             </div>
-            <div class="col">
+            <div class="col has-padding">
                 <div class="img-wrap img-4-x-3">
                     <img src="<?php echo get_stylesheet_directory_uri() . "/assets/images/young-adults-at-coffee-shop.jpg"; ?>" />
                 </div>
@@ -66,12 +66,12 @@
 <section class="has-padded-height">
         <div class="container has-half">
             <div class="grid vertical-align is-vertical-on-mobile">
-                <div class="col">
+                <div class="col has-padding">
                     <div class="img-wrap img-4-x-3">
                         <img src="<?php echo get_stylesheet_directory_uri() . "/assets/images/small-group-with-bibles.jpg"; ?>" />
                     </div>
                 </div>
-                <div class="col">
+                <div class="col has-padding">
                     <h2>Kingdom Life Groups</h2>
                     <h3>Every Sunday Morning 9:30 am - 10:30 am</h3>
                     <p>Sunday mornings are a great time to not only come together for worship but certainly a time of growth and fellowship that you simply don’t get other days of the week. We believe that outside of Sunday mornings, life happens in ways that are sometimes easier dealt with in smaller group sessions. Kingdom Life offers groups to cover practical life experiences using biblical solutions. These groups are much smaller than the Sunday morning worship service and meet 8 weeks for one hour weekly.</p>
@@ -84,12 +84,12 @@
 <section class="has-padded-height">
     <div class="container has-half">
         <div class="grid is-vertical-on-mobile">
-            <div class="col">
+            <div class="col has-padding">
                 <h2>Worship &amp; Arts</h2>
                 <p> It is the vision of the worship & arts ministry to provide an atmosphere in which the presence of God is experienced by all through worship and praise unto our God in heaven, our Lord and Savior, Jesus Christ. We seek to provide a means for those with performing arts gifting, talent, and desire to grow and mature both in artistry and spiritually by way of discipline and stewardship according to the Word of God. Singers, musicians, dancers, poets, mime dancers, actors, and actresses are all welcome.</p> 
                 <a class="btn btn-outlined red" href="<?php echo home_url( 'forms/volunteer' ); ?>">Worship With Us</a>
             </div>
-            <div class="col">
+            <div class="col has-padding">
                 <div class="img-wrap img-4-x-3">
                     <img src="<?php echo get_stylesheet_directory_uri() . "/assets/images/dance-ministry-black-gold-robes.jpg"; ?>" />
                 </div>
