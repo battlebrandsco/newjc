445e70d797dcea9c59eb5e2643bc3755444354c8
Marcus Battle <themarcusbattle@gmail.com>
2018-09-23 01:48:14 -0400

    Updated the membership page template

--- themes/newjc/page-membership.php
+++ themes/newjc/page-membership.php
@@ -9,22 +9,12 @@
         </div>
     </div>
 </section>
-<section>
+
+<section id="main">
     <div class="container is-half">
-        <p>We are so honored that you are interested in being a member of New Jerusalem Cathedral.We understand that selecting membership in a church is a major commitment and a vital part of spiritual development. We strongly believe that it is beneficial for men and women to fellowship together in unity, as Psalms 133:1 reminds us. Our Bishop, Dr. Kevin Williams has provided the 3 Core Membership Values:</p>
-        <h2 class="has-text-align-center">3 Core Membership Values</h2>
-        <ul>
-            <li><strong>A holistic and biblical theology.</strong> As a member you must come to church and hear the word of God as well as self study to build your spirit and your relationship with God.</li>
-            <li><strong>Application theology relationship.</strong> Even as faith comes by hearing the word of God. If it is not applied and lived you will fail in your christian walk and relationship with God. Genesis 1 teaches word must be met with action to prove true understanding.</li>
-            <li><strong>An evangelism theology relationship.</strong> Go ye into all the world and preach the gospel is Christ’s command and demand. A christian that does not share Christ exhibits no true relationship with Him. Acts 1 shows that we have been given power to evangelize Christ to three areas:
-                <ul>
-                    <li>Self</li>
-                    <li>Family</li>
-                    <li>Country</li>
-                </ul>
-            </li>
-        </ul>
-        <p class="has-text-align-center"><a class="btn" href="<?php echo home_url('forms/membership'); ?>">Become A Member</a></p>
+        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+            <?php the_content(); ?>
+        <?php endwhile; endif; ?>
     </div>
 </section>
 
